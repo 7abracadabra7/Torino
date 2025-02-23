f89cfb2672bd14e03e263bc2b4647f78b186c490
Nahid Khalili <Nahid_khalili@ymail.com>
2025-02-23 20:10:08 +0330

    add slider

--- components/molecules/ImageSlider.js
+++ components/molecules/ImageSlider.js
@@ -1,6 +1,14 @@
 "use client";
 import React, { useState } from "react";
 import styles from "./ImageSlider.module.css";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+import { EffectCards } from "swiper/modules";
 
 const ImageSlider = () => {
   const initialImages = [
@@ -11,59 +19,88 @@ const ImageSlider = () => {
   ];
 
   const [images, setImages] = useState(initialImages);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  // const [currentIndex, setCurrentIndex] = useState(0);
 
-  const nextSlide = () => {
-    setImages((prevImages) => {
-      const newImages = [...prevImages];
-      const movedImage = newImages.shift();
-      newImages.push(movedImage); 
-      console.log(currentIndex + 1, newImages);
-      return newImages;
-    });
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % 4); 
-  };
+  // const nextSlide = () => {
+  //   setImages((prevImages) => {
+  //     const newImages = [...prevImages];
+  //     const movedImage = newImages.shift();
+  //     newImages.push(movedImage);
+  //     console.log(currentIndex + 1, newImages);
+  //     return newImages;
+  //   });
+  //   setCurrentIndex((prevIndex) => (prevIndex + 1) % 4);
+  // };
 
-  const prevSlide = () => {
-  setImages((prevImages) => {
-      const newImages = [...prevImages];
-      const movedImage = newImages.pop(); 
-      newImages.unshift(movedImage); 
-      return newImages;
-    });
-    setCurrentIndex((prevIndex) => {
-      if (prevIndex === 0) {
-        return 3;
-      }
-      return prevIndex - 1; 
-    });
-  };
+  // const prevSlide = () => {
+  //   setImages((prevImages) => {
+  //     const newImages = [...prevImages];
+  //     const movedImage = newImages.pop();
+  //     newImages.unshift(movedImage);
+  //     return newImages;
+  //   });
+  //   setCurrentIndex((prevIndex) => {
+  //     if (prevIndex === 0) {
+  //       return 3;
+  //     }
+  //     return prevIndex - 1;
+  //   });
+  // };
 
   return (
-    <div className={styles.sliderContainer}>
-      <div className={styles.slides}>
+    <>
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className={styles.swiper}
+      >
         {images.map((image, index) => (
-          <div
-            className={`${styles.slide} ${
-              index === currentIndex ? styles.active : ""
-            }`}
-            key={index}
-            style={{
-              backgroundImage: `url(${image})`,
-            }}
-          >
-            <span className={styles.slideNumber}>Slide {currentIndex }</span>
-          </div>
+          // <div
+          //   className={`${styles.slide} ${
+          //     index === currentIndex ? styles.active : ""
+          //   }`}
+          //   key={index}
+          //   style={{
+          //     backgroundImage: `url(${image})`,
+          //   }}
+          // >
+
+          <SwiperSlide className={styles["swiper-slide"]}>
+          
+            <img src={image} />
+          </SwiperSlide>
+          // </div>
         ))}
-      </div>
-      <button className={styles.prevButton} onClick={prevSlide}>
-        ❮
-      </button>
-      <button className={styles.nextButton} onClick={nextSlide}>
-        ❯
-      </button>
-    </div>
+
+      </Swiper>
+    </>
+    //
+
+    // <div className={styles.sliderContainer}>
+    //   <div className={styles.slides}>
+    //     {images.map((image, index) => (
+    //       <div
+    //         className={`${styles.slide} ${
+    //           index === currentIndex ? styles.active : ""
+    //         }`}
+    //         key={index}
+    //         style={{
+    //           backgroundImage: `url(${image})`,
+    //         }}
+    //       >
+    //         <span className={styles.slideNumber}>Slide {currentIndex }</span>
+    //       </div>
+    //     ))}
+    //   </div>
+    //   <button className={styles.prevButton} onClick={prevSlide}>
+    //     ❮
+    //   </button>
+    //   <button className={styles.nextButton} onClick={nextSlide}>
+    //     ❯
+    //   </button>
+    // </div>
   );
 };
 
-export default ImageSlider;                                                                                                          
+export default ImageSlider;
